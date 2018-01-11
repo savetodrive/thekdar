728d6f2eb03719be678fa677882945f5a800a8bd
samundrak <samundrak@yahoo.com>
2018-01-12 00:54:35 +0545

    Add event emit

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const Thekdar = require("./core/Thekdar");
 const Task = require("./core/Task");
 
 const thekdar = new Thekdar();
-for (let i = 0; i < Thekdar.MAX_TASK_PER_WORKER; i++) {
-  const task = {};
-  task.getType = () => Task.TYPE_FORK;
-  task.setId = id => (task.id = id);
-  task.getId = () => task.id;
-  thekdar.addTask(task);
-}
-for (let i = 0; i < Thekdar.MAX_TASK_PER_WORKER; i++) {
-  const task = {};
-  task.getType = () => Task.TYPE_FORK;
-  task.setId = id => (task.id = id);
-  task.getId = () => task.id;
-  thekdar.addTask(task);
-}
-thekdar.emit("add", "sas");
-thekdar.on("add", () => {
-  console.log("hello world");
+thekdar.on("add", d => {
+  console.log(d);
 });
+for (let i = 0; i < 2; i++) {
+  const task = {};
+  task.getType = () => Task.TYPE_FORK;
+  task.setId = id => (task.id = id);
+  task.getId = () => task.id;
+  thekdar.addTask(task);
+}
+// for (let i = 0; i < Thekdar.MAX_TASK_PER_WORKER; i++) {
+//   const task = {};
+//   task.getType = () => Task.TYPE_FORK;
+//   task.setId = id => (task.id = id);
+//   task.getId = () => task.id;
+//   thekdar.addTask(task);
+// }
